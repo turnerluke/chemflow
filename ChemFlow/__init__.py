42d74928334934a4937d0dce77199412ba8ad229
turnerluke <70869027+turnerluke@users.noreply.github.com>
2021-08-04 21:17:56 -0500

    Create __init__.py

---
+++ ChemFlow/__init__.py
@@ -0,0 +1,9 @@
+"""
+    ChemFlow
+    ~~~~~~~~
+    ChemFlow is Python module/package to perform sequential chemical process calculations,
+    for high-throughput evaluation of chemical process designs.
+
+    :copyright: 2021 by Turner Luke
+    :license: BSD, see LICENSE for more details.
+"""
