4edac6d079d703357d8af2038f9069eaed6e5e19
turnerluke <70869027+turnerluke@users.noreply.github.com>
2021-08-05 12:31:03 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+            name='chemflow',
+            url='https://github.com/turnerluke/chemflow',
+            author='Turner LUke',
+            author_email='tluke@wisc.edu',
+            # Needed to actually package something
+            packages=['measure'],
+            # Needed for dependencies
+            install_requires=['numpy'],
+            # *strongly* suggested for sharing
+            version='0.1',
+            # The license can be anything you like
+            license='MIT',
+            description='chemflow a Python package for quick sequential chemical process design calculations.'
+            # long_description=open('README.txt').read(),
+    )
